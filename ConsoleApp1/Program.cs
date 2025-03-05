125128d5b95d0a2d02e15fc2c7f0004ab18a642a
Seleez <bartekczernek10@gmail.com>
2025-03-05 14:41:11 +0100

    ogólna funkcja zamien

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Program
-    {
+    internal class Program{
+
+        static void Zamien<T>(ref T a, ref T b)
+        {
+            T bufor = a;
+            a = b;
+            b = bufor;
+        }
+  
         static void Main(string[] args)
         {
+            double liczba1 = 10, liczba2 = 20;
+            Console.WriteLine($"{liczba1}\t{liczba2}");
+            Zamien(ref liczba1 , ref liczba2); 
+            Console.WriteLine($"{liczba1}\t{liczba2}");
+
+            bool l1 = true, l2 = false;
+            Console.WriteLine($"{l1}\t{l2}");
+            Zamien(ref l1, ref l2);
+            Console.WriteLine($"{l1}\t{l2}");
+
         }
     }
 }
