6cd77df6a7216783cef57abe21a32a1e929684c6
Seleez <bartekczernek10@gmail.com>
2025-03-05 14:55:09 +0100

    generic class warehouse and computer

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,26 +6,76 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Program{
+    public class Warehouse<T>
+    {
+        private List<T> products = new List<T>();
 
-        static void Zamien<T>(ref T a, ref T b)
+        public void addItem(T item)
         {
-            T bufor = a;
-            a = b;
-            b = bufor;
+            products.Add(item);
         }
-  
+        public void removeItem(T item)
+        {
+            products.Remove(item);
+        }
+        public List<T> GetAllProducts() => products;
+
+    }
+    public class Computer
+    {
+        public string producer { get; set; }
+        public double CPU { get; set; }
+        public int RAM { get; set; }
+    };
+        internal class Program
+        {
+
+            static void Zamien<T>(ref T a, ref T b)
+            {
+                T bufor = a;
+                a = b;
+                b = bufor;
+            }
+        
         static void Main(string[] args)
         {
-            double liczba1 = 10, liczba2 = 20;
-            Console.WriteLine($"{liczba1}\t{liczba2}");
-            Zamien(ref liczba1 , ref liczba2); 
-            Console.WriteLine($"{liczba1}\t{liczba2}");
+            //double liczba1 = 10, liczba2 = 20;
+            //Console.WriteLine($"{liczba1}\t{liczba2}");
+            //Zamien(ref liczba1 , ref liczba2); 
+            //Console.WriteLine($"{liczba1}\t{liczba2}");
 
-            bool l1 = true, l2 = false;
-            Console.WriteLine($"{l1}\t{l2}");
-            Zamien(ref l1, ref l2);
-            Console.WriteLine($"{l1}\t{l2}");
+            //bool l1 = true, l2 = false;
+            //Console.WriteLine($"{l1}\t{l2}");
+            //Zamien(ref l1, ref l2);
+            //Console.WriteLine($"{l1}\t{l2}");
+
+            var warehouse = new Warehouse<Computer>();
+
+            warehouse.addItem(new Computer
+            {
+                producer = "Dell",
+                CPU = 4.5,
+                RAM = 32
+            });
+            warehouse.addItem(new Computer
+            {
+                producer = "Hp",
+                CPU = 3.0,
+                RAM = 20
+            });
+            warehouse.addItem(new Computer
+            {
+                producer = "GG",
+                CPU = 4.5,
+                RAM = 32
+            });
+
+            foreach (var computer in warehouse.GetAllProducts())
+            {
+                Console.WriteLine($"Producer: {computer.producer}");
+                Console.WriteLine($"CPU: {computer.CPU}");
+                Console.WriteLine($"RAM: {computer.RAM}");
+            }
 
         }
     }
