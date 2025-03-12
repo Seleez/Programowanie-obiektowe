54a66424f0fc03e98a9d56d42af767fd4dcd3769
Seleez <bartekczernek10@gmail.com>
2025-03-12 15:11:13 +0100

    IProducts

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsoleApp1
-{
+{   
+    public interface IProducts
+    {
+        //string Name { get; set; }
+        //decimal Price { get; set; }
+
+        void DisplayInfo();
+    }
     public class Warehouse<T>
     {
         private List<T> products = new List<T>();
@@ -14,20 +23,44 @@ namespace ConsoleApp1
         {
             products.Add(item);
         }
-        public void removeItem(T item)
+        public void removeItem(int id)
         {
-            products.Remove(item);
+            products.RemoveAt(id);
         }
         public List<T> GetAllProducts() => products;
 
     }
-    public class Computer
+    public class Computer : IProducts
     {
         public string producer { get; set; }
         public double CPU { get; set; }
         public int RAM { get; set; }
     };
-        internal class Program
+
+    public class Electronics : IProducts
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int WarrantyMonths { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Nazwa: {this.Name} Cena: {this.Price} Gwarancja: {this.WarrantyMonths}");
+        }
+    }
+
+    public class Furniture : IProducts
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Material { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Nazwa: {this.Name} Cena: {this.Price} materiał: {this.Material}");
+        }
+    }
+    internal class Program
         {
 
             static void Zamien<T>(ref T a, ref T b)
@@ -49,7 +82,7 @@ namespace ConsoleApp1
             //Zamien(ref l1, ref l2);
             //Console.WriteLine($"{l1}\t{l2}");
 
-            var warehouse = new Warehouse<Computer>();
+            var warehouse = new Warehouse<IProducts>();
 
             warehouse.addItem(new Computer
             {
@@ -69,14 +102,53 @@ namespace ConsoleApp1
                 CPU = 4.5,
                 RAM = 32
             });
-
-            foreach (var computer in warehouse.GetAllProducts())
+            warehouse.addItem(new Electronics
             {
-                Console.WriteLine($"Producer: {computer.producer}");
-                Console.WriteLine($"CPU: {computer.CPU}");
-                Console.WriteLine($"RAM: {computer.RAM}");
+                Name = "Czajnik",
+                Price = 150,
+                WarrantyMonths = 24
+            });
+            warehouse.addItem(new Electronics
+            {
+                Name = "Żelazko",
+                Price = 300,
+                WarrantyMonths = 36
+            });
+            warehouse.addItem(new Electronics
+            {
+                Name = "Blender",
+                Price = 100,
+                WarrantyMonths = 6
+            });
+            warehouse.addItem(new Furniture
+            {
+                Name = "Stół",
+                Price = 1000,
+                Material = "drewno"
+            });
+            warehouse.addItem(new Furniture
+            {
+                Name = "Sofa",
+                Price = 2500,
+                Material = "drewno, materiał"
+            });
+            warehouse.addItem(new Furniture
+            {
+                Name = "krzesło",
+                Price = 100,
+                Material = "drewno"
+            });
+            foreach (var product in warehouse.GetAllProducts())
+            {
+                if(product is Computer)
+                {
+                    var computer = product as Computer;
+                    Console.WriteLine($"Producer: {computer.producer}");
+                    Console.WriteLine($"CPU: {computer.CPU}");
+                    Console.WriteLine($"RAM: {computer.RAM}");
+                }
+                
             }
-
         }
     }
 }
