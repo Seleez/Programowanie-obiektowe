260770691c3f53743426a72de83a8550d127826d
Seleez <bartekczernek10@gmail.com>
2025-04-16 15:05:28 +0200

    strategy

---
+++ strategia/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace strategia
+{
+    public interface IStrategy
+    {
+        void attack(Character cel);
+    }
+    public class Character
+    {
+        public string name { get; set; }
+        public IStrategy strategy { get; set; }
+
+        public Character(string name, IStrategy strategy)
+        {
+            this.name = name;
+            this.strategy = strategy;
+        }
+
+        public void changeStrategy(IStrategy newStrategy)
+        {
+            strategy = newStrategy;
+        }
+        public void attack(Character cel) {
+            strategy.attack(cel);
+        }
+    }
+
+    public class Magic : IStrategy
+    {
+        public void attack(Character cel)
+        {
+            Console.WriteLine($"You cast spell on {cel}");
+        }
+    }
+    public class Melee : IStrategy
+    {
+        public void attack(Character cel)
+        {
+            Console.WriteLine($"You hit {cel.name} with a sword");
+        }
+    }
+    public class Ranged : IStrategy
+    {
+        public void attack(Character cel)
+        {
+            Console.WriteLine($"You shoot {cel.name} with an arrow");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Magic magicAttack = new Magic();
+            Melee meleeAttack = new Melee();
+            Ranged rangedAttack = new Ranged();
+            Character Enemy = new Character("Enemy", rangedAttack);
+            Character MC = new Character("s", meleeAttack);
+            MC.attack(Enemy);
+            MC.changeStrategy(rangedAttack);
+            MC.attack(Enemy);
+        }
+    }
+}
